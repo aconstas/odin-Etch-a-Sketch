835a3224160008712795b3059e52a94cfdfb2574
aconstas <82304192+aconstas@users.noreply.github.com>
2023-08-29 10:56:06 -0700

    Put logic in createGrid() to run on user input.

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const gridSpace = document.querySelector('#container');
 
-for (i = 1; i <= 16; i++) {
-    const box = document.createElement('div');
-    box.setAttribute('id', i);
-    box.setAttribute('class', 'topBox');
-    gridSpace.appendChild(box);
+function createGrid(num) {
+    gridSpace.innerHTML = '';
+    for (i = 1; i <= num ** 2; i++) {
+        const box = document.createElement('div');
+        box.setAttribute('id', i);
+        box.setAttribute('class', 'box');
+        box.style.width = `calc(1/(${num}) * 100%)`;
+        box.style.height = `calc(1/${num} * 100%)`;
+        gridSpace.appendChild(box);
+    }
+    
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.classList.add('hovered');
+    }))
 }
 
-for (i = 1; i <= 16; i++) {
-    const topBox = document.querySelectorAll('.topBox');
-    const columnBoxes = document.createElement('div')
-    columnBoxes.setAttribute('class', 'column-boxes')
-    topBox.forEach((firstBox) => firstBox.appendChild(columnBoxes));
-}
+createGrid(16);
 
-//create gridSpace width and height according to user input
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let blockInput = window.prompt('Enter desired number of blocks per side.');
+    console.log(typeof (blockInput));
+    if (blockInput > 100) {
+        alert('Please enter a numerical value under 100.')
+    } else {
+        createGrid(blockInput);
+    }
+});
+//  there might be something wrong with the calc() sometimes the widths and heights
+//  do not match up.. there tends to be one extra box that is left in its own row at
+//  the bottom
+
+//  the issue was with the border! I removed the border
